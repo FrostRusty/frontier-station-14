dcf85de114ebc3ec324239c95f223707934d49ee
Rusty <tarotskier@protonmail.com>
2024-10-08 15:27:08 -0700

    More removal to mapchecker to give ships more freedom

--- .github/mapchecker/config.py
+++ .github/mapchecker/config.py
@@ -1,24 +1,8 @@
 # List of matchers that are always illegal to use. These always supercede CONDITIONALLY_ILLEGAL_MATCHES.
 ILLEGAL_MATCHES = [
-    "DO NOT MAP",
     "DEBUG",
     "Admeme",
     "SalvageShuttleMarker",
     "FTLPoint"
 ]
-# List of matchers that are illegal to use, unless the map is a ship and the ship belongs to the keyed shipyard.
-CONDITIONALLY_ILLEGAL_MATCHES = {
-    "Syndicate": [
-        "Plastitanium",  # And also on blackmarket ships cause syndicate.
-        "ButtonFrameCautionSecurity",  # Decal.
-        "ShuttleGun",
-    ],
-    "BlackMarket": [
-        "Plastitanium",  # And also on blackmarket ships cause syndicate.
-        "ButtonFrameCautionSecurity",  # Decal.
-        "ShuttleGun",
-    ],
-    "Sr": [
-        "ButtonFrameCautionSecurity",  # Decal.
-    ]
-}
+
